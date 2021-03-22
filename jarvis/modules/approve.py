e47d953aac5e77c906e4228309d333d82fa7eb13
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-03-22 15:51:49 +0530

    Fixes

--- jarvis/modules/approve.py
+++ jarvis/modules/approve.py
@@ -3,14 +3,14 @@ from jarvis import dispatcher, SUDO_USERS
 from jarvis.modules.helper_funcs.extraction import extract_user
 from telegram.ext import run_async, CallbackQueryHandler
 import jarvis.modules.sql.approve_sql as sql
-from jarvis.modules.helper_funcs.chat_status import (bot_admin, user_admin, can_promote)
+from jarvis.modules.helper_funcs.chat_status import (bot_admin, user_admin, promote_permission)
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import Update, Bot, Message, Chat, User
 from typing import Optional, List
 
 
 @user_admin
-@can_promote
+@promote_permission
 @run_async
 def approve(bot: Bot, update: Update, args: List[str]) -> str:
 	 message = update.effective_message
@@ -32,7 +32,7 @@ def approve(bot: Bot, update: Update, args: List[str]) -> str:
 	 message.reply_text(f"[{member.user['first_name']}](tg://user?id={member.user['id']}) has been approved in {chat_title}! They will now be ignored by automated admin actions like locks, blocklists, and antiflood.", parse_mode=ParseMode.MARKDOWN)
      
 @user_admin
-@can_promote
+@promote_permission
 @run_async
 def disapprove(bot: Bot, update: Update, args: List[str]) -> str:
 	 message = update.effective_message
